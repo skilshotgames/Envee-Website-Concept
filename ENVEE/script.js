9a9f4405a3aa7506a97a19125700c5e01a8cabe8
skilshotgames <75539869+skilshotgames@users.noreply.github.com>
2024-12-02 15:01:59 +0200

    Add files via upload

---
+++ ENVEE/script.js
@@ -0,0 +1,5 @@
+document.getElementById('booking-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Your appointment has been booked successfully!');
+  });
+  
