14f459a78a90948c09dc1b033ccd3d21a258d7e6
skilshotgames <75539869+skilshotgames@users.noreply.github.com>
2024-12-03 09:19:34 +0200

    Delete ENVEE directory

--- ENVEE/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('booking-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Your appointment has been booked successfully!');
-  });
-  
